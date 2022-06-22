321d71482737055d5b6ba1f49a79dd9b824ed833
Anthony Jameson <ajamesonan@gmail.com>
2022-06-22 18:55:22 -0400

    test send

--- June_Web_Fund/Week1/Day1/dojopractice.js
+++ June_Web_Fund/Week1/Day1/dojopractice.js
@@ -1,19 +1,16 @@
-var num1 = 20;
-var num2 = 5;
-if (num1 < num2) {
-    num2 = num2 * num1;
-} else {
-    num1 = num1 / num2;
-    if (num1 < num2){
-        num1 = num1 * 2;
-    } else if (num1 == num2){
-        num2 = num1 * num2;
-    } else {
-        num2 = num2 * 2;
+function getTotal(arrayOfNumbers) {
+    
+    var sum = arrayOfNumbers[0];
+      
+    for(var i=0; i<arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+      console.log("the current sum is: " + sum); 
     }
-}
-console.log(num1);
-console.log(num2);
-
-
+      
+    console.log("the total is: " + sum);
+      
+  }
+      
+  getTotal([1, 3, 5]);
+  
 
