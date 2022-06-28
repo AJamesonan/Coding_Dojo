45c84a477c0cba61b21d549c3a6038836fbf0ce8
Anthony Jameson <ajamesonan@gmail.com>
2022-06-28 10:52:11 -0400

    week2 day1

---
+++ June_Web_Fund/Practice/dojopractice.js
@@ -0,0 +1,15 @@
+function example(element) {
+  console.log("element clicked", element);
+}
+
+function reverse(arr){
+  var temparr = undefined;
+  var i= arr.length-1;
+  while(i>=0){
+    i--;
+    console.log()
+  }
+}
+
+var buttstuff = [10,9,8,7,6,5,4,3,2,1]
+
--- June_Web_Fund/dojopractice.js
+++ June_Web_Fund/dojopractice.js
@@ -1,19 +1,15 @@
-function isPal(arr) {
-  for(var left=0; left<arr.length/2; left++) {
-      var right = arr.length-1-left;
-      console.log(left, right);
-      if(arr[left] != arr[right]) {
-          return "Not a pal-indrome!";
-      }
-  }
-  return "Pal-indrome!";
+function example(element) {
+  console.log("element clicked", element);
 }
-  
-var result1 = isPal( [1, 1, 2, 2, 1] );
-console.log(result1);
-  
-var result2 = isPal( [3, 2, 1, 1, 2, 3] );
-console.log(result2);
 
+function reverse(arr){
+  var temparr = undefined;
+  var i= arr.length-1;
+  while(i>=0){
+    i--;
+    console.log()
+  }
+}
 
+var buttstuff = [10,9,8,7,6,5,4,3,2,1]
 
